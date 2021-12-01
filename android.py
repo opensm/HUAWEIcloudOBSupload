382445a947f7465b70d8c2ce2d43599ce4b6ac2b
engine-oper <smxblysq@qq.com>
2021-12-01 11:06:46 +0800

    同步代码

--- android.py
+++ android.py
@@ -91,7 +91,7 @@ def zipfile(read_file, out_dir, task_time, remote_dir):
         print("{}不存在，请上传！".format(notice_current))
         sys.exit(1)
     file_dir = os.path.join(out_dir, SUB_PATH)
-    os.rename(notice_current, file_dir)
+    os.rename(notice_current, os.path.join(file_dir,'notice.json'))
     if not os.path.exists(read_file):
         raise Exception("{0} 文件不存在！".format(read_file))
     if not os.path.exists(os.path.join(
